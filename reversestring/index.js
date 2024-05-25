031ab31abc15f9c408839133ff166f0b33309f4e
Amusah <henryamusah@gmail.com>
2024-05-25 20:28:17 +0000

    DSA exercise 1 reversing a string

---
+++ reversestring/index.js
@@ -0,0 +1,18 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+// solution 1
+function reverse(str) {
+  // return str.split('').reverse().join();
+  // return str.split('').reverse().join();
+  const arr = str.split('');
+  arr.reverse();
+  return arr.join('');
+}
+
+module.exports = reverse;
